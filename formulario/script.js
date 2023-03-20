d631fc99f1a7e8c1b2fd30be5aaab3bd82e54ce0
Kauã Xavier <kauaxavier032@gmail.com>
2023-03-20 11:11:32 -0300

    finalizado

--- formulario/script.js
+++ formulario/script.js
@@ -3,15 +3,26 @@ let passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/
 let btn_submit = document.getElementById("login")
 let email = document.getElementById("for_email")
 let password = document.getElementById("for_password")
+let errorEmail = document.getElementById("error_email")
+let errorPassword = document.getElementById("error_password")
 
 email.addEventListener("click", () =>{
     email.style.border = "none"
+    errorEmail.style.display = "none"
+    errorEmail.innerHTML = ""
+})
+password.addEventListener("click", () =>{
+    password.style.border = "none"
+    errorPassword.style.display = "none"
+    errorPassword.innerHTML = ""
 })
 
 btn_submit.addEventListener("click", (e)=>{
     e.preventDefault()
-
+    
     if(emailRegex.test(email.value) == true && passwordRegex.test(password.value) == true){
+        errorEmail.style.display = "none"
+        errorPassword.style.display = "none"
         alert("Login feito com sucesso!")
     }
     else if(emailRegex.test(email.value) == true){
@@ -19,11 +30,15 @@ btn_submit.addEventListener("click", (e)=>{
     }
     else if(emailRegex.test(email.value) == false){
         email.style.border = "2px solid red"
+        errorEmail.style.display = "block"
+        errorEmail.innerHTML = "Email Inválido"
     }
     if(passwordRegex.test(password.value) == true){
         password.style.border = "2px solid green"
     }
     else if(passwordRegex.test(password.value) == false){
         password.style.border = "2px solid red"
+        errorPassword.style.display = "block"
+        errorPassword.innerHTML = "Deve conter (@, 4, A, a)"
     }
 })
